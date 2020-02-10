41a72e58cdaa5da06a0f86ffb49d385460430052
matt <mpilkou@gmail.com>
2020-02-10 08:41:33 +0300

    init task 2

---
+++ task 2/solution.py
@@ -0,0 +1,23 @@
+class Version:
+    def __init__(self, version):
+        pass
+
+
+def main():
+    to_test = [
+        ('1.0.0', '2.0.0'),
+        ('1.0.0', '1.42.0'),
+        ('1.2.0', '1.2.42'),
+        ('1.1.0-alpha', '1.2.0-alpha.1'),
+        ('1.0.1b', '1.0.10-alpha.beta'),
+        ('1.0.0-rc.1', '1.0.0'),
+    ]
+
+    for version_1, version_2 in to_test:
+        assert Version(version_1) < Version(version_2), 'le failed'
+        assert Version(version_2) > Version(version_1), 'ge failed'
+        assert Version(version_2) != Version(version_1), 'neq failed'
+
+
+if __name__ == "__main__":
+    main()
