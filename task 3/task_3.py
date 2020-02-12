f65ca0981b6faff8cad1b7bee985ac469d58df8c
matt <mpilkou@gmail.com>
2020-02-12 22:03:44 +0300

    (solution) add Lock & fix pylint i in for

--- task 3/task_3.py
+++ task 3/task_3.py
@@ -1,18 +1,20 @@
-from threading import Thread
+from threading import Thread, Lock
 
 a = 0
 
 
-def function(arg):
+def function(arg, lock):
     global a
-    for _ in range(arg):
-        a += 1
+    with lock:
+        for _ in range(arg):
+            a += 1
 
 
 def main():
     threads = []
-    for i in range(5):
-        thread = Thread(target=function, args=(1000000,))
+    lock = Lock()
+    for _ in range(5):
+        thread = Thread(target=function, args=(1000000,lock))
         thread.start()
         threads.append(thread)
 
