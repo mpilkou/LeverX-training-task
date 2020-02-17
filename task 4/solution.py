f3951c634c6f8733be4ca335bb023a93dd6cdeac
matt <mpilkou@gmail.com>
2020-02-18 00:20:24 +0300

    add changed task1

--- task 4/solution.py
+++ task 4/solution.py
@@ -4,28 +4,15 @@ import json
 
 class Controller:
 
-    def __init__(self, students_path, rooms_path, output = 'json'):
+    def __init__(self, students_path, rooms_path):
         
         # chech files existens
         if self.check_file_not_exists(students_path) and self.check_file_not_exists(rooms_path):
             exit()
 
-        # check format of output
-        if not (output in ['json','xml']):
-            print('Incorrect format of output try xml of json')
-            exit()
-        
         self.students_path = students_path
         self.rooms_path = rooms_path
-        self.output = output
         self.rooms = {}
-
-    # give output
-    def export(self):
-        if self.output == 'xml':
-            self.export_xml()
-        else:
-            self.export_json()
 
     # add students to rooms
     def add_rooms_from_json(self):
@@ -40,14 +27,12 @@ class Controller:
             for student in json.load(myfile):
                 self.rooms[student['room']].addStudent(Student(student['id'],student['name'],student['room']))
 
-    def export_json(self):
-        with open('solution.json', 'w') as outfile:
-            for r in self.rooms.values():
-                json.dump(r, outfile, indent=2, default=self.my_jsonEncoder)
+    def export_json(self, output_path = 'solution.json'):
+        with open(output_path, 'w') as outfile:
+            json.dump(list(self.rooms.values()), outfile, indent=2, default=self.my_jsonEncoder)
     
-    
-    def export_xml(self):
-        with open('solution.xml', 'w') as outfile:
+    def export_xml(self, output_path = 'solution.xml'):
+        with open(output_path, 'w') as outfile:
             for r in self.rooms.values():
                 outfile.write('<room id=\"{r_id}\"> \n<name>{r_name}</name> \n\t<students>\n'.format(r_id = r.id, r_name = r.name))
                 
@@ -124,9 +109,14 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) < 3:
-        print('give 3 arguments - path to students.json, path to rooms.json, output (json,xml)')
+        print('give 3 arguments - path to students.json, path to rooms.json, output_path path (<name>.json,xml)')
         exit()
 
-    controll = Controller(students_path = sys.argv[1], rooms_path = sys.argv[2], output = sys.argv[3])
+    controll = Controller(students_path = sys.argv[1], rooms_path = sys.argv[2])
     controll.add_rooms_from_json()
-    controll.export()
+
+    if sys.argv[3][-3:] == 'xml':
+        controll.export_xml(output_path = sys.argv[3])
+    else:
+        controll.export_json(output_path = sys.argv[3])
+        
