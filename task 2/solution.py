875dc0c92e937169d0fb4c698c84bb84b99ae224
matt <mpilkou@gmail.com>
2020-02-17 23:23:38 +0300

    fix python syntax

--- task 2/solution.py
+++ task 2/solution.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.6
 
-# in python 3.6 i don't need to use functools.total_ordering
+# in python 3.6 I don't need to use functools.total_ordering
 
 class Version:
     def __init__(self, version):
@@ -16,7 +16,7 @@ class Version:
 
     def __eq__(self, other):
         if len(self.version) == len(other.version):
-            for i in range(len(self.version)):
+            for i,_ in enumerate(self.version):
                 if not (self.version[i] == other.version[i]):
                     return False
             return True
@@ -73,6 +73,7 @@ def main():
         ('1.1.0-alpha', '1.2.0-alpha.1'),
         ('1.0.1b', '1.0.10-alpha.beta'),
         ('1.0.0-rc.1', '1.0.0'),
+        ('1.0.0','1.0.00')
     ]
 
     for version_1, version_2 in to_test:
