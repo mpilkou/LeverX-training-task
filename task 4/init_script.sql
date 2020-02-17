495afe722796827d99b8441ba298eeb0ce2e66d8
matt <mpilkou@gmail.com>
2020-02-18 00:38:01 +0300

    init sql sctipt

---
+++ task 4/init_script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE leverx_task4_db COLLATE utf8_general_ci;
+CREATE TABLE Rooms (
+    id int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50)
+);
+CREATE TABLE Students (
+    id int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    sex varchar(1) NOT NULL,
+    birthday date NOT NULL,
+    room_id int UNSIGNED NOT NULL,
+    FOREIGN KEY (room_id) REFERENCES Rooms(id)
+);
