0d5e8b16a57522e92a0608db670107ef6d59d831
matt <mpilkou@gmail.com>
2020-02-18 18:44:54 +0300

    fix id != 0

--- task 4/init_script.sql
+++ task 4/init_script.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE leverx_task4_db COLLATE utf8_general_ci;
 
 CREATE TABLE Rooms (
-    id int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id int UNSIGNED NOT NULL PRIMARY KEY,
     name varchar(50) NOT NULL
 );
 CREATE TABLE Students (
-    id int UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id int UNSIGNED NOT NULL PRIMARY KEY,
     name varchar(50) NOT NULL,
     sex varchar(1) NOT NULL,
     birthday date NOT NULL,
