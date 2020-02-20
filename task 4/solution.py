18a157b7b73272129c63ad9f3f12ad98faf01fe9
matt <mpilkou@gmail.com>
2020-02-20 17:23:41 +0300

    add argparse

--- task 4/solution.py
+++ task 4/solution.py
@@ -2,7 +2,6 @@
 
 import json
 import typing
-import mysql.connector
 from classes import Student, Room
 from controllers import FilesController, DBController
 from models import Model
@@ -10,25 +9,31 @@ from models import Model
 
 if __name__ == "__main__":
     # check input
-    import sys
+    import argparse
 
-    if len(sys.argv) < 3:
-        print('give 3 arguments - path to students.json, path to rooms.json, output_path path (<name>.json,xml)')
-        exit()
+    parser = argparse.ArgumentParser(description='give 3 arguments - path to students.json, path to rooms.json, output_path path (<name>.json,xml)')
+
+    parser.add_argument('students', metavar='path to students.json', type=str, help='path to students.json')
+    parser.add_argument('rooms', metavar='path to rooms.json', type=str, help='path to rooms. json')
+    parser.add_argument('output_file', metavar='output file (xml or json)', type=str, help='output file ( xml or json)')
+
+    args = parser.parse_args()
+
+
 
     # init controller
-    file_controll = FilesController(students_path = sys.argv[1], rooms_path = sys.argv[2])
+    file_controll = FilesController(students_path = args.students, rooms_path = args.rooms)
 
     # students to rooms & file export
     file_controll.concatinate_students_to_rooms_from_json()
 
-    if sys.argv[3][-3:] == 'xml':
-        file_controll.export_xml(output_path = sys.argv[3])
+    if args.output_file[3][-3:] == 'xml':
+        file_controll.export_xml(output_path = args.output_file[3][-3:])
     else:
-        file_controll.export_json(output_path = sys.argv[3])
+        file_controll.export_json(output_path = args.output_file[3][-3:])
 
 
-    db_controll = DBController(students_path = sys.argv[1], rooms_path = sys.argv[2])
+    db_controll = DBController(students_path = args.students, rooms_path = args.rooms)
     # db import
     db_controll.import_to_db()
 
