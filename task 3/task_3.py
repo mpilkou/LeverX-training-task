e394e9366769bc3a1e439769f6a305b80e1ac706
matt <mpilkou@gmail.com>
2020-02-26 19:13:53 +0300

    add previous solution with (may be solution as meaning)

--- task 3/task_3.py
+++ task 3/task_3.py
@@ -2,19 +2,45 @@ from threading import Thread, Lock
 from concurrent.futures import ThreadPoolExecutor
 
 
-def function(arg):
+a = 0
+
+def function1(arg, lock):
+    tmp = 0
+
+    for _ in range(arg):
+        tmp += 1
+
+    with lock:
+        global a
+        a += tmp
+
+
+def function2(arg):
     a = 0
     for _ in range(arg):
         a += 1
     return a
 
-
 def main():
     
+    # using previos solution
+    threads = []
+    lock = Lock()
+    for _ in range(5):
+        thread = Thread(target=function1, args=(1000000,lock))
+        thread.start()
+        threads.append(thread)
+
+    [t.join() for t in threads]
+    print("----------------------", a)  # ???
+    
+
+    # using thread pool
     with ThreadPoolExecutor(max_workers=5) as executor:
-        future = executor.submit(function, 5000000)
+        future = executor.submit(function2, 5000000)
         future.result()
 
         print("----------------------", future.result())
+
 
 main()
