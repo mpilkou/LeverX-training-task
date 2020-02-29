afc5b5cf254251abecc0a57cd3a5016933df06ee
matt <mpilkou@gmail.com>
2020-02-29 17:42:08 +0300

    add models

--- task 5/courses_project/course_app/models.py
+++ task 5/courses_project/course_app/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
 # Create your models here.
+# base.User
+# AbstractUser
+#user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Teacher(AbstractUser):
+
+    class Meta:
+        permissions = [
+            ("add_course", "add course"),
+            ("add_students", "add students"),
+            ("add_teachers", "add teachers"),
+            ("teacher", "teacher permisions"),
+        ]    
+
+class Student(AbstractUser):
+
+    class Meta:
+        permissions = [
+            ("add_homework", "add homework"),
+            ("student", "student permisions"),
+        ]
+
+class Course(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    teachers = models.ManyToManyField(Teacher)
+    students = models.ManyToManyField(Student)
+
+class Lection(models.Model):
+
+    title = models.CharField(max_length=80, blank=False)
+    presentation = models.TextField(blank=False)
+
+
+    course = models.ForeignKey(Course, on_delete = models.PROTECT)
+
+class Homework(models.Model):
+    txt = models.CharField(max_length=200, blank=False)
+    comment = models.CharField(max_length=40, blank=True)
+    mark = models.DecimalField(max_digits=3, decimal_places=0, blank=True)
+
+    lection = models.ForeignKey(Lection, on_delete=models.PROTECT)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+
--- task 5/courses_project/course_app/views.py
+++ task 5/courses_project/course_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 # Create your views here.
+
+@login_required(login_url='/login/')
+def get_courses(request):
+    pass
+
+#@permission_required('polls.add_choice', login_url='/login/')#raise_exception=True)
