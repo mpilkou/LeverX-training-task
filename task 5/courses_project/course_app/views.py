4cb14e3b4596df81be3ee1dc27c61ea4160fe2b5
matt <mpilkou@gmail.com>
2020-03-01 01:03:55 +0300

    add /api & support get and posts

--- task 5/courses_project/course_app/views.py
+++ task 5/courses_project/course_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from course_app import models
 #from . import models
 
@@ -8,13 +11,23 @@ from course_app import models
 
 #@login_required(login_url='login')
 def login(request):
-    pass
+    print(request.GET)
     # get_or_create
     #models.Teacher.objects.create_user('test', 'test@test.com', 'test')
+    #tea = models.Teacher()
+    #tea.save()
     #return redirect('post_detail')
     #return {'aaa':'aaaa'}
 
-def signup(reqest):
-    pass
+@api_view(['GET', 'POST'])
+def signup(request):
+
+    if request.method == 'POST':
+        a = request.data
+
+    else:
+        a = request.data
+        
+    return Response(a)
 
 #@permission_required('polls.add_choice', login_url='/login/')#raise_exception=True)
